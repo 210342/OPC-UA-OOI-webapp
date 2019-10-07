9865c36aedb850f5596434e6f08745950fa2a91f
210342 <210342@edu.p.lodz.pl>
2019-10-07 15:29:36 +0200

    Property printing model

---
+++ MessageParsing/NonPrintableProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MessageParsing
+{
+    public class NonPrintableProperty : IProperty
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+        public decimal? XPosition => null;
+        public decimal? YPosition => null;
+        public Color? BackgroundColor => null;
+        public Color? FontColor => null;
+
+        public NonPrintableProperty(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}
