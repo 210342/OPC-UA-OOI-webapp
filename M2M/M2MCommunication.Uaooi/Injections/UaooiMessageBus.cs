7206f91a4142795fe7eb325e87e4add1ddb4580a
210342 <210342@edu.p.lodz.pl>
2019-12-10 21:25:56 +0100

    Removed redundant method

--- M2M/M2MCommunication.Uaooi/Injections/UaooiMessageBus.cs
+++ M2M/M2MCommunication.Uaooi/Injections/UaooiMessageBus.cs
@@ -44,8 +44,6 @@ namespace M2MCommunication.Uaooi.Injections
         /// <exception cref="ValueRankOutOfRangeException"></exception>
         public void Initialise(Action<object, ISubscription> onSubsctiptionAdded)
         {
-            AssertComponentsAreNotNull();
-
             _logger?.LogInfo("Initialising subscription logic");
             (BindingFactory as ISubscriptionFactory).SubscriptionAdded += new EventHandler<ISubscription>((sender, subscription) =>
             {
@@ -67,41 +65,13 @@ namespace M2MCommunication.Uaooi.Injections
         /// <exception cref="ValueRankOutOfRangeException"></exception>
         public async Task InitialiseAsync(Action<object, ISubscription> onSubsctiptionAdded)
         {
-            await Task.Run(() => Initialise( onSubsctiptionAdded));
+            await Task.Run(() => Initialise(onSubsctiptionAdded));
         }
 
         public void RefreshConfiguration()
         {
             _logger?.LogInfo("Reloading consumer configuration and restarting communication");
             Start();
-        }
-
-        private void AssertComponentsAreNotNull()
-        {
-            if (ConfigurationFactory is null)
-            {
-                var exception = new ComponentNotInitialisedException(nameof(ConfigurationFactory));
-                _logger?.LogError(exception, exception.Message);
-                throw exception;
-            }
-            else if (EncodingFactory is null)
-            {
-                var exception = new ComponentNotInitialisedException(nameof(EncodingFactory));
-                _logger?.LogError(exception, exception.Message);
-                throw exception;
-            }
-            else if (BindingFactory is null)
-            {
-                var exception = new ComponentNotInitialisedException(nameof(BindingFactory));
-                _logger?.LogError(exception, exception.Message);
-                throw exception;
-            }
-            else if (MessageHandlerFactory is null)
-            {
-                var exception = new ComponentNotInitialisedException(nameof(MessageHandlerFactory));
-                _logger?.LogError(exception, exception.Message);
-                throw exception;
-            }
         }
     }
 }
